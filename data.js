5addd6ff797358992c714ffe6532095cfac99382
Dimitar <dimitar.nizamov@klevret.com>
2021-03-29 20:17:10 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,55 @@
+
+const data = {
+  threads: {
+    t1: {
+      '.key': 't1',
+      title: 'My first thread',
+      publishedAt: '',
+      posts: {
+        'p1': 'p1'
+      },
+      userId: ''
+    }
+  },
+  posts: {
+    p1: {
+      '.key': 'p1',
+      publishedAt: '',
+      userId: '',
+      text: 'Hello',
+      threadId: 't1'
+    }
+  },
+  users: {
+    u1: {
+      name: 'Alice',
+      '.key': 'u1',
+      threads: {
+        t1: 't1'
+      },
+      posts: {
+        p1: 'p1'
+      }
+    }
+  },
+  forums: {
+    f1: {
+      '.key': 'f1',
+      name: 'Fishing',
+      description: '',
+      categoryId: 'c1',
+      threads: {
+        t1: 't1'
+      }
+    }
+  },
+  categories: {
+    c1: {
+      '.key': 'c1',
+      name: 'Discussions',
+      forums: {
+        f1: 'f1'
+      }
+    }
+  }
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>Welcome to the forum</h1>
+  </main>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    };
+  }
+};
+</script>
